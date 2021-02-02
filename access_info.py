cc509c2f8ac9304d2cd013e130cddc031c72bc7a
Ali Parvizi <snparvizi75@gmailc.om>
2021-02-02 16:19:06 +0330

    update finger table

--- access_info.py
+++ access_info.py
@@ -8,16 +8,12 @@ class AccessInfo:
         self.port = port
         self.id = id
 
+    def get_uri(self):
+        return "ws://%s:%d" % (self.address, self.port)
+
     def __repr__(self) -> str:
         return json.dumps({
             'address': self.address,
             'port': self.port,
             'id': self.id
         })
-
-
-t = AccessInfo("localhost", 3242, 3)
-d = jsonpickle.encode(t)
-print(d)
-print(type(d))
-print(jsonpickle.decode(d))
--- node.py
+++ node.py
@@ -1,26 +1,24 @@
 import asyncio
-from time import sleep
+
 import websockets
 import jsonpickle
 from access_info import AccessInfo
 import random
 from threading import Thread
-import math
 import json
+import time
 
 
 class Node:
-
-    _MAX = 32   # max numbers of nodes in the ring
+    _MAX = 32  # max numbers of nodes in the ring
 
     def __init__(self, ai, chord_access_info):
-        self.finger_table = []
-        self.nxt = ai
-        self.prv = ai
-        self.cai = chord_access_info
-        self.ai = ai
+        self.finger_table = []  # finger table
+        self.nxt = ai  # next node access info (address, port, id)
+        self.prv = ai  # previous node access info (address, port, id)
+        self.cai = chord_access_info  # a chord node that this node will be joind to
+        self.ai = ai  # self access info
 
-    ### setters and getters start
     def set_nxt(self, nxt):
         self.nxt = nxt
 
@@ -32,42 +30,44 @@ class Node:
 
     def get_prv(self):
         return self.prv
-    ### setters and getteres end
-    
+
     def get_range(self):
-        if self.prv:
-            
-            # range is (prvId, currentId]
-            # or (prvId, _MAX] if currentID < prvId
-            if self.ai.id > self.prv.id:
-                _start = self.prv.id + 1
-                _end = self.ai.id + 1
-            elif self.ai.id < self.prv.id:
-                _start = self.prv + 1
-                _end = self._MAX + 1
-            else:
-                _start = 1
-                _end = 33
+        """
+        :return: a range of keys, that might be saved on this node
+        """
 
-            return range(_start, _end)
-        
-        # if no prv, only one node is in the ring
-        # so all ranges are acceptable
-        return range(1, 33)
-            
+        # range is (prvId, currentId]
+        # or (prvId, _MAX] if currentID < prvId
+        if self.ai.id > self.prv.id:
+            _start = self.prv.id + 1
+            _end = self.ai.id + 1
+        elif self.ai.id < self.prv.id:
+            _start = self.prv + 1
+            _end = self._MAX + 1
+        else:
+            _start = 1
+            _end = self._MAX
 
-    async def remote_locate(self, _chord, key):
-        async with websockets.connect("ws://%s:%d" % (_chord.address,
-         _chord.port)) as websocket:
+        return range(_start, _end)
 
+    @staticmethod
+    async def remote_locate(_chord, key):
+        """
+        :param _chord: a node in the ring
+        :param key: key to ask the location of, from the given _chord
+        :return: a _chord AI(accessInfo) where the given key is saved
+        """
+        async with websockets.connect(_chord.get_uri()) as ws:
+
+            # we intend to call the locate function on the given _chord
             _data = {
                 'func_name': 'locate',
-                'args': (),
-                'kwargs': {'key': key}
+                'kwargs': {'key': key},
+                'args': ()
             }
 
-            await websocket.send(json.dumps(_data))
-            _result = await websocket.recv()
+            await ws.send(json.dumps(_data))
+            _result = await ws.recv()
 
             if _result is not None:
                 result = jsonpickle.decode(_result)
@@ -95,9 +95,9 @@ class Node:
         for i in range(len(self.finger_table) - 1):
             if self.finger_table[i].id <= key <= self.finger_table[i + 1].id:
                 return await self.remote_locate(self.finger_table[i], key)
-        
+
         _chord_id = - 1
-        _chord= None
+        _chord = None
 
         for ft in self.finger_table:
             if ft.id > _chord_id:
@@ -105,60 +105,30 @@ class Node:
 
         if not _chord:
             raise Exception("yja ridim")
-            
+
         return await self.remote_locate(_chord, key)
-        
-    async def run(self, websocket, path):
-        """
-        func_data = json.loads(func_str)
-
-        function = func_data.get('func_name')
-        args = func_data.get('args')
-        kwargs = func_data.get('kwargs')
-
-        """
-
-        func_str = await websocket.recv()
-
-        func_data = json.loads(func_str)
-        func_name = func_data.get('func_name')
-        func = eval("self.%s" % func_name)
-        args = func_data.get('args')
-        kwargs = func_data.get('kwargs')
-
-        _return = func(*args, **kwargs)
-
-        print("[NODE %d] exceuted %s with args %s and kwargs %s" % (self.ai.id, func_name, json.dumps(args), json.dumps(kwargs)))
-
-        await websocket.send(json.dumps(_return))
-        
-        return json.dumps(_return)
-
-    def hello(self, name):
-        return "Hello %s" % name
 
     async def if_not_my_range_nxt_range(self, q):
         if q in self.get_range():
             key_q = self.ai
         else:
-            nxt = self.nxt
-            async with websockets.connect("ws://%s:%d" % (nxt.address, nxt.port)) as websocket:
+            async with websockets.connect(self.nxt.get_uri()) as ws:
                 _data = {
                     'func_name': 'if_not_my_range_nxt_range',
                     'args': (),
                     'kwargs': {'q': q}
                 }
 
-                await websocket.send(json.dumps(_data))
-                _result = await websocket.recv()
+                await ws.send(json.dumps(_data))
+                _result = await ws.recv()
 
                 if _result is not None:
-                    result = json.loads(_result)
+                    result = jsonpickle.decode(_result)
                 else:
                     result = None
 
                 key_q = result
-        print("[NODE %d] " % self.ai.id, key_q.address, key_q.port, key_q.id)
+
         return key_q
 
     async def update_finger_table(self):
@@ -177,10 +147,154 @@ class Node:
         self.finger_table = []
 
         for i in range(5):
-            q = self.ai.id + 2**(i)
+            q = self.ai.id + 2 ** i % self._MAX
 
             key_q = await self.if_not_my_range_nxt_range(q)
             self.finger_table.append(key_q)
+
+    def get_finger_table(self):
+        return self.finger_table
+
+    async def run(self, websocket, path):
+        """
+        func_data = json.loads(func_str)
+
+        function = func_data.get('func_name')
+        args = func_data.get('args')
+        kwargs = func_data.get('kwargs')
+
+        """
+
+        while True:
+            func_str = await websocket.recv()
+            func_data = json.loads(func_str)
+
+            func_name = func_data.get('func_name')
+            func = eval("self.%s" % func_name)
+            args = func_data.get('args')
+            kwargs = func_data.get('kwargs')
+
+            try:
+                # the given function is an async, we need to await it
+                _return = await func(*args, **kwargs)
+            except TypeError:
+                _return = func(*args, **kwargs)
+
+            print("[NODE %d] executed %s with args %s and kwargs %s" % (
+                self.ai.id, func_name, json.dumps(args), json.dumps(kwargs)))
+
+            await websocket.send(jsonpickle.encode(_return))
+            return jsonpickle.encode(_return)
+
+    @staticmethod
+    async def test_run_function(_ai):
+        """
+        tests the functionality of run function a given ai node
+        :param _ai: remove chord ai
+        :return: None
+        """
+        # sync function test
+        print("[TEST] -----------> Testing Sync Function Call")
+        async with websockets.connect(_ai.get_uri()) as ws:
+            _data = {
+                'func_name': 'hello',
+                'args': (),
+                'kwargs': {"name": "sina"}
+            }
+
+            print("[TEST] Sending Data")
+            await ws.send(json.dumps(_data))
+            print("[TEST] Waiting for Data")
+            _result = await ws.recv()
+            result = jsonpickle.decode(_result)
+
+            print("[TEST] result: ", result)
+            assert result == "Hello sina"
+        print("[TEST] ############ Testing Sync Function Call --- end")
+
+        # async function test
+        print("[TEST] -----------> Testing Async Function Call")
+        async with websockets.connect(_ai.get_uri()) as ws:
+            _data = {
+                'func_name': 'a_hello',
+                'args': (),
+                'kwargs': {"name": "sina"}
+            }
+
+            print("[TEST] Sending Data")
+            await ws.send(json.dumps(_data))
+            print("[TEST] Waiting for Data")
+            _result = await ws.recv()
+            result = jsonpickle.decode(_result)
+
+            print("[TEST] result: ", result)
+            assert result == "Async Hello sina"
+        print("[TEST] ############ Testing Async Function Call")
+
+    @staticmethod
+    async def test_finger_table(_ai):
+        """
+        test finger table creation
+        :param _ai: remove chord ai
+        :return: None
+        """
+        # sync function test
+        print("[TEST] -----------> Update Finger Table")
+        async with websockets.connect(_ai.get_uri()) as ws:
+            _data = {
+                'func_name': 'update_finger_table',
+                'args': (),
+                'kwargs': {}
+            }
+
+            print("[TEST] Sending Data")
+            await ws.send(json.dumps(_data))
+            print("[TEST] Waiting for Data")
+            _result = await ws.recv()
+            result = jsonpickle.decode(_result)
+
+            print("[TEST] result: ", result)
+            assert result == None
+        print("[TEST] ############ Update Finger Table")
+
+        print("[TEST] -----------> Get Finger Table")
+        async with websockets.connect(_ai.get_uri()) as ws:
+            _data = {
+                'func_name': 'get_finger_table',
+                'args': (),
+                'kwargs': {}
+            }
+
+            print("[TEST] Sending Data")
+            await ws.send(json.dumps(_data))
+            print("[TEST] Waiting for Data")
+            _result = await ws.recv()
+            result = jsonpickle.decode(_result)
+
+            print("[TEST] result: ", result)
+        print("[TEST] ############ Get Finger Table")
+
+    def test(self):
+        # this process may be run from a thread
+        # get a new event loop
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+        print("[TEST] ==================>")
+        # run function test
+        asyncio.get_event_loop().run_until_complete(self.test_run_function(self.ai))
+        asyncio.get_event_loop().run_until_complete(self.test_finger_table(self.ai))
+        print("[TEST] ===================")
+
+    @staticmethod
+    def hello(name):
+        """Used for testing sync function calls"""
+        return "Hello %s" % name
+
+    @staticmethod
+    async def a_hello(name):
+        """Used for testing async function calls"""
+        return "Async Hello %s" % name
 
     def start(self):
         """
@@ -194,56 +308,33 @@ class Node:
             pass
         else:
             pass
-        
-        
 
+        # this process may be run from a thread
+        # get a new event loop
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        
-        asyncio.get_event_loop().run_until_complete(self.update_finger_table())
-        
+
         start_server = websockets.serve(self.run, self.ai.address, self.ai.port)
         asyncio.get_event_loop().run_until_complete(start_server)
 
         print("[NODE : %d] running node on %s:%d" % (self.ai.id, self.ai.address, self.ai.port))
+        print("[NODE : %d] running self test")
+
+        _test = Thread(target=self.test)
+        _test.start()
+
         print("[NODE %d] waiting for commands" % self.ai.id)
 
         asyncio.get_event_loop().run_forever()
 
-    def print_finger_table(self):
-        return "[NODE %d] Finget Table: " % self.ai.id, self.finger_table
 
 def node_manager(_ai):
     _node = Node(_ai, None)
     _node.start()
 
 
-async def test(_ai):
-    
-    async with websockets.connect("ws://%s:%d" % (_ai.address, _ai.port)) as websocket:
-        
-        _data = {
-            'func_name': 'print_finger_table',
-            'args': (),
-            'kwargs': {}
-        }
-
-        await websocket.send(json.dumps(_data))
-        _result = await websocket.recv()
-
-        if _result is not None:
-            result = json.loads(_result)
-        else:
-            result = None
-
-        print(result)
-
 if __name__ == "__main__":
-    _ai = AccessInfo("localhost", 9090, random.randint(1,32))
-    _t = Thread(target=node_manager, args=(_ai, ))
+    _ai = AccessInfo("localhost", 9090, random.randint(1, 32))
+    _t = Thread(target=node_manager, args=(_ai,))
     _t.start()
-    import time
-    time.sleep(1)
-    asyncio.get_event_loop().run_until_complete(test(_ai))
-    
     _t.join()
